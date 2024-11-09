ceeb501474e6fe7ae72684a44756c2e34b8544fc
Manpreet Singh <vickyarora.singh@gmail.com>
2024-11-08 23:52:11 -0500

    cleaned Welcome.jsx code

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { useGLTF, Html } from '@react-three/drei';
+import { useGLTF } from '@react-three/drei';
 
-export function Welcome({ onTransformClick }) {
+export function Welcome() {
     const { nodes, materials } = useGLTF('./models/helmet.glb');
 
     return (
-
-        <group dispose={null}>
-            {/* Helmet 3D Model */}
-            <group scale={0.01}>
-                <group position={[-2.634, -17.631, -33.11]}>
-                    <mesh geometry={nodes.parts_parts_0.geometry} material={materials.parts} />
-                    <mesh geometry={nodes.parts_L2_0.geometry} material={materials.material} />
-                    <mesh geometry={nodes.parts_L1_0.geometry} material={materials.material_2} />
-                </group>
-            </group>        </group>
+        <group scale={0.01}>
+            <mesh geometry={nodes.parts_parts_0.geometry} material={materials.parts} />
+            <mesh geometry={nodes.parts_L2_0.geometry} material={materials.material} />
+            <mesh geometry={nodes.parts_L1_0.geometry} material={materials.material_2} />
+        </group>
     );
 }
 
